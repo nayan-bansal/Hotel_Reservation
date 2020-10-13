ff98417c2ccb39f1e6231367c6d406ed3967b88f
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 16:31:17 +0530

    Added: Welcome Message to Hotel Reservation System

---
+++ Hotel_Reservation_Main.java
@@ -0,0 +1,16 @@
+package Hotel;
+
+public class Hotel_Reservation_Main {
+
+	
+	public static void main(String[] args) {
+		
+		//UC_1: Welcome Message
+		
+		System.out.println("Welcome to Hotel Reservation System!!");
+		
+		
+	}
+	
+	
+}
