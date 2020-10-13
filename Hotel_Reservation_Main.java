b0a7421866c5361229671e556c19ea4b7f603949
nayan-bansal <nayan.7991@gmail.com>
2020-10-14 03:23:41 +0530

    Added: Cheapest Hotel on a given Day

--- Hotel_Reservation_Main.java
+++ Hotel_Reservation_Main.java
@@ -1,11 +1,19 @@
 package Hotel;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Hotel_Reservation_Main {
 
+	private static final Scanner scan = new Scanner(System.in);
+	static HotelLog log = new HotelLog();
+	
 	private static void addHotelRegularPrice() {
 		
 		//Adding The List of Hotels
-		HotelLog log = new HotelLog();
 		
 		Hotel LakeWood = new Hotel("LakeWood", 110, 90);
 		log.addHotelBook(LakeWood);
@@ -19,21 +27,59 @@ public class Hotel_Reservation_Main {
 		//Printing Hotel List and Rates
 		log.printHotel();
 		
+		
+	}
+	
+	private static Hotel findcheapesthotel(String start_date, String end_date) {
+		//Start and End Dates
+		Date startdate = null;
+		Date enddate = null;
+		
+		try {
+		 startdate = new SimpleDateFormat("ddMMMyyyy").parse(start_date);
+		 enddate = new SimpleDateFormat("ddMMMyyyy").parse(end_date);
+		
+		}
+		
+		catch(ParseException e){
+			
+			e.printStackTrace();
+		}
+		
+		long time = (enddate.getTime() - startdate.getTime() )/1000/60/60/24 + 1;
+
+		Hotel cheapesthotel = log.hotelbook.stream().sorted(Comparator.comparing(Hotel :: getWeekday)).findFirst().orElse(null);	
+		
+		System.out.println(cheapesthotel.getWeekday());
+		long total_amount = time*cheapesthotel.getWeekday();
+		
+		cheapesthotel.setTotalPrice(total_amount);
+		return cheapesthotel;
 	}
 	
 	
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception {
 		
-		//UC_1: Welcome Message
+		//UC_0: Welcome Message
 		
 		System.out.println("Welcome to Hotel Reservation System!!");
 		
-		//UC_2: Adding Hotel to the Log Book and Printing Hotel Name with Rates for Weekend and Weekdays
+		//UC_1: Adding Hotel to the Log Book and Printing Hotel Name with Rates for Weekend and Weekdays
 		addHotelRegularPrice();
-	
-	
+		
+		HotelLog log = new HotelLog();
+		
+		//UC_2: Finding the cheapest Hotel in the given date
+		System.out.println("Enter the Check in Date in ddMMMYYYY Format: ");
+		String start_date = scan.next();
+		
+		System.out.println("Enter the Check Out Date in ddMMMYYYY Format: ");
+		String end_date = scan.next();
+		
+		Hotel cheap = findcheapesthotel(start_date, end_date);
+		System.out.println(cheap.getHotelName()+" is the Cheapest Hotel with Price $ "+cheap.getTotalPrice());
+		
 	}
-
-	
 }
