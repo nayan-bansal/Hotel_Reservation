7b4ad43b63b5072826fe9098515ff94e42adcb30
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 21:46:13 +0530

    Added: The Hotels in the MAin Program

--- Hotel_Reservation_Main.java
+++ Hotel_Reservation_Main.java
@@ -2,6 +2,26 @@ package Hotel;
 
 public class Hotel_Reservation_Main {
 
+	private static void addHotelRegularPrice() {
+		
+		//Adding The List of Hotels
+		HotelLog log = new HotelLog();
+		
+		Hotel LakeWood = new Hotel("LakeWood", 110, 90);
+		log.addHotelBook(LakeWood);
+		
+		Hotel BridgeWood = new Hotel("BridgeWood", 160, 60);
+		log.addHotelBook(BridgeWood);
+		
+		Hotel RidgeWood = new Hotel("RidgeWood", 220, 150);
+		log.addHotelBook(RidgeWood);
+		
+		//Printing Hotel List and Rates
+		log.printHotel();
+		
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
@@ -9,8 +29,11 @@ public class Hotel_Reservation_Main {
 		
 		System.out.println("Welcome to Hotel Reservation System!!");
 		
-		
-	}
+		//UC_2: Adding Hotel to the Log Book and Printing Hotel Name with Rates for Weekend and Weekdays
+		addHotelRegularPrice();
 	
+	
+	}
+
 	
 }
